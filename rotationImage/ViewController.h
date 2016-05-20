a9edfb20212d5a727835201aa56e0452b53ef326
xabita <mutskxabaa@gmail.com>
2016-05-20 18:57:11 -0500

    initial

--- rotationImage/ViewController.h
+++ rotationImage/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+- (IBAction)handleRotation:(UIGestureRecognizer*)sender;
 
 @end
 
--- rotationImage/ViewController.m
+++ rotationImage/ViewController.m
@@ -24,4 +24,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)handleRotation:(UIGestureRecognizer*)sender {
+  NSLog(@"holaaa");
+    
+}
 @end

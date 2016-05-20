63b2fea56e3abe162d7198d27cc04414cdaa7730
xabita <mutskxabaa@gmail.com>
2016-05-20 17:33:27 -0500

    Initial Commit

---
+++ rotationImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rotationImage
+//
+//  Created by Elizabeth Martínez Gómez on 20/05/16.
+//  Copyright © 2016 Elizabeth Martínez Gómez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ rotationImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  rotationImage
+//
+//  Created by Elizabeth Martínez Gómez on 20/05/16.
+//  Copyright © 2016 Elizabeth Martínez Gómez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
